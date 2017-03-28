1971b5c9554ddf82e7626e928a098cb7e29f830b
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-03-28 16:16:00 +0530

    Update Transportation_on_vacation.js

--- Transportation_on_vacation.js
+++ Transportation_on_vacation.js
@@ -2,3 +2,5 @@ function rentalCarCost(days) {
   var discount = (days <= 2) ? 0 : ((days >= 7 ) ? 50 : 20 );
   return (days * 40) - discount;
 }
+
+//http://www.codewars.com/kata/transportation-on-vacation

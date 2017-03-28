72c51decf3f0a842cb4e1e1866608f9f5ebc90cb
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-03-28 16:49:36 +0530

    Update Basic_Mathematical_Operations.js

--- Basic_Mathematical_Operations.js
+++ Basic_Mathematical_Operations.js
@@ -1,3 +1,5 @@
+//I completed the task in two ways , could you please tell which one is efficient ??
+
 function basicOp(operation, value1, value2) {
   var listOfOperations = {
       '+': value1 + value2,
@@ -6,6 +8,25 @@ function basicOp(operation, value1, value2) {
       '/': value1 / value2
   }
   return listOfOperations[operation];
+}
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+      case '+':
+          return value1 + value2;
+          break;
+      case '-':
+          return value1 -value2;
+          break;
+      case '*':
+          return value1 * value2;
+          break;
+      case '/':
+          return value1 / value2;
+          break;
+      default:
+          return 0;
+  }
 }
 
 //http://www.codewars.com/kata/basic-mathematical-operations

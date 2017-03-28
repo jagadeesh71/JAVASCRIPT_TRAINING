b50d520b4d4afaa3baa7820a610c02914d81bdf4
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-03-28 16:45:45 +0530

    Update Basic_Mathematical_Operations.js

--- Basic_Mathematical_Operations.js
+++ Basic_Mathematical_Operations.js
@@ -1,5 +1,11 @@
 function basicOp(operation, value1, value2) {
-  return eval(value1 + operation + value2);
+  var listOfOperations = {
+      '+': value1 + value2,
+      '-': value1 - value2,
+      '*': value1 * value2,
+      '/': value1 / value2
+  }
+  return listOfOperations[operation];
 }
 
 //http://www.codewars.com/kata/basic-mathematical-operations

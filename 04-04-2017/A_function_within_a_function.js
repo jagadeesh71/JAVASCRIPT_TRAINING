a892ffbe4832a28380d4912b91b80d4e9e9fb12f
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-04 00:35:05 +0530

    Create A_function_within_a_function.js

---
+++ 04-04-2017/A_function_within_a_function.js
@@ -0,0 +1,8 @@
+// return a function that returns n
+function always (n) {
+  return function () {
+    return n;
+  }
+}
+
+//http://www.codewars.com/kata/a-function-within-a-function/train/javascript

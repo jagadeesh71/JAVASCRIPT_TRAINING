30b1bdab480bc99683dbc32368d570ce6780104e
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-04 12:46:58 +0530

    Update Partition_On.js

--- 30-03-2017/Partition_On.js
+++ 30-03-2017/Partition_On.js
@@ -1,7 +1,23 @@
-//This program is not yet completed I am WORKING on it.
-
+//I have seen a different variant of doing this using apply at 14 instead of using two for loops. What it 
+//will exactly do and for what purpose we can use apply.
 function partitionOn(pred, items) {
+  var evenNumbers = items.filter(pred);
+  var oddNumbers = items.filter(function (number) {
+    return !pred(number);
+  });
+  items.length = 0;
+  for(var item = 0; item < oddNumbers.length; item++) {
+    items.push(oddNumbers[item]);
+  }
+  for(var index = 0; index < oddNumbers.length; index++) {
+    items.push(evenNumbers[index]);
+  }
+  //items.push.apply(items, oddNumbers.concat(evenNumbers));
+  return oddNumbers.length;
 }
 
+
+//In line 9 AND 12 ,I declared variables item and index with the keyword var right? my quesition is will the scope of item and index be 
+//available through out the function or just for the for loop??
 
 //http://www.codewars.com/kata/partition-on

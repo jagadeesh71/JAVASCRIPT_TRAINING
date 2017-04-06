73a386640e8d3b6d425e2c0b5db744a677c46366
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-06 23:20:23 +0530

    Update Prefill_an_Array.js

--- 04-04-2017/Prefill_an_Array.js
+++ 04-04-2017/Prefill_an_Array.js
@@ -1,1 +1,22 @@
 
+function prefill(n, v) {
+  var i = 1,arrayList = [],
+      pushElement = function () {
+      if (i <= parseInt(n, 10)) {
+        arrayList.push(v);
+        i++;
+        return arrayList = pushElement();
+      } else {
+        return arrayList;
+      }
+    };
+  if (n === 0) {
+    return arrayList;
+  } else if (isNaN(n) || n < 0 || typeof(n) === "boolean" || n % 1 !== 0) {
+    throw new TypeError(n+' is invalid');
+  } else {
+    return pushElement();
+  }
+}
+
+//http://www.codewars.com/kata/prefill-an-array/train/javascript

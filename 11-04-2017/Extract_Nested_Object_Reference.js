4ae05d0ffd7a6c60bf59ad6dd006b006c68730d5
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-13 20:16:14 +0530

    Update Extract_Nested_Object_Reference.js

--- 11-04-2017/Extract_Nested_Object_Reference.js
+++ 11-04-2017/Extract_Nested_Object_Reference.js
@@ -2,14 +2,14 @@
 // otherwise return undefined
 Object.prototype.hash = function(string) {
   var objProperties = string.split('.'),
-      finalObj = this[objProperties[0]];
-  for (var i = 1; i < objProperties.length; i++) {
-    finalObj = finalObj[objProperties[i]];
-    if(!finalObj) { 
-      return finalObj;
+      value = this;
+  for (var i = 0; i < objProperties.length; i++) {
+    value = value[objProperties[i]];
+    if(!value) { 
+      return value;
     }
   }
-  return finalObj;
+  return value;
 }
 
 //http://www.codewars.com/kata/extract-nested-object-reference/train/javascript

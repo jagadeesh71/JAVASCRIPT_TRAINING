950ab0e5e3acb8823b67086240308c2f8939f16b
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-14 01:12:38 +0530

    Create new_with_apply.js

---
+++ 11-04-2017/new_with_apply.js
@@ -0,0 +1,8 @@
+function construct(Class) {
+  var obj = Object.create(Class.prototype),
+      args= Array.prototype.slice.call(arguments, 1);
+  Class.apply(obj, args);
+  return obj;
+}
+
+//http://www.codewars.com/kata/new-with-apply/train/javascript

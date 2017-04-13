5e0e0f65bd8adc5b9c6aba0bd76332a4de36ad5c
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-14 01:35:37 +0530

    Delete Printing_Array_elements_with_Comma_delimiters.js

--- Printing_Array_elements_with_Comma_delimiters.js
+++
@@ -1,6 +0,0 @@
-// I didn't know before that we can separate the array elements by any delimiter.
-function printArray(array) {
-  return array.join(';');
-}
-
-//http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters

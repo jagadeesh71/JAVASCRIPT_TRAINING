54bf8c0af34305e432f938d903e0bd4f4413144e
jagadeesh avvaru <javvaru@allianceglobalservices.com>
2017-04-19 16:13:09 +0530

    Update new_with_apply.js

--- 11-04-2017/new_with_apply.js
+++ 11-04-2017/new_with_apply.js
@@ -1,6 +1,6 @@
 function construct(Class) {
   var obj = Object.create(Class.prototype),
-      args= Array.prototype.slice.call(arguments, 1);
+      args= [].slice.call(arguments, 1);
   Class.apply(obj, args);
   return obj;
 }
